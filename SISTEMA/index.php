d1c9fb8333e1b59ecedc4802dc97d0734f24d1ae
davi <dv.gasparino@gmail.com>
2021-05-26 21:30:12 -0300

    ajuste css

--- SISTEMA/index.php
+++ SISTEMA/index.php
@@ -1,19 +1,19 @@
 <?php require_once("header.php"); ?>
 
-    <section class="fluid-container p-4">
-        <div class="container">
+    <section class="fluid-container p-2">
+        <div class="container justify-content-md-center col-9">
             <div class="row">
                 <p class="text-center"> Faça sua certificação em <span class="strong-title">6 etapas</span> </p>
             </div>
         </div>
     </section>
 
-    <section class="fluid-container pt-4">
-        <div class="container">
+    <section class="fluid-container pt-2">
+        <div class="container justify-content-md-center col-9">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="card-item-block p-4">
-                        <h3 class="mb-3">Cadastra</h3>
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">Cadastra</h3>
                         <div class="row">
                             <div class="col-md-3">
                                 <span class="big-number">1</span>
@@ -31,8 +31,8 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card-item-block p-4">
-                        <h3 class="mb-3">ORÇA</h3>
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">ORÇA</h3>
                         <div class="row">
                             <div class="col-md-3">
                                 <span class="big-number">2</span>
@@ -49,8 +49,8 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card-item-block p-4">
-                        <h3 class="mb-3">PLANEJA</h3>
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">PLANEJA</h3>
                         <div class="row">
                             <div class="col-md-3">
                                 <span class="big-number">3</span>
@@ -67,8 +67,8 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card-item-block p-4">
-                        <h3 class="mb-3">AVALIA</h3>
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">AVALIA</h3>
                         <div class="row">
                             <div class="col-md-3">
                                 <span class="big-number">4</span>
@@ -85,8 +85,8 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card-item-block p-4">
-                        <h3 class="mb-3">REVISA</h3>
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">REVISA</h3>
                         <div class="row">
                             <div class="col-md-3">
                                 <span class="big-number">5</span>
@@ -103,8 +103,8 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="card-item-block p-4">
-                        <h3 class="mb-3">CERTIFICA</h3>
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">CERTIFICA</h3>
                         <div class="row">
                             <div class="col-md-3">
                                 <span class="big-number">6</span>
