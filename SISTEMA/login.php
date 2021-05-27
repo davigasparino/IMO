a2237e460562559d00d57773f85f49fb5e286d18
davi <dv.gasparino@gmail.com>
2021-05-26 23:14:24 -0300

    login

--- SISTEMA/login.php
+++ SISTEMA/login.php
@@ -1,228 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <title>IMO do Brasil - Certificadora</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <head>
+        <title>IMO do Brasil - Certificadora</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;700;800;900&display=swap" rel="stylesheet">
-    <style>
-        @import "css/style.css";
-    </style>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark top-header top-phones">
-    <div class="container justify-content-md-center col-9">
-        <div class="navbar-brand" href="#">
-            Olá Nome do Usuário
-        </div>
-        <div class="collapse navbar-collapse top-main">
-            <ul class="navbar-nav nav-icons">
-                <li class="nav-item notificate-grid">
-                    <span class="material-icons icon">
-                        question_answer
-                    </span>
-                    <span class="notifications">
-                        23
-                    </span>
-                    <div class="notifications-container">
-                        <ul class="navbar-nav nav-notifications">
-                            <li class="navbar-item">
-                                <a class="li-content">
-                                    <div class="title">
-                                        <span class="material-icons icon">
-                                             person
-                                        </span>
-                                        <span class="username">Usuário 1</span>
-                                    </div>
-                                    <div class="text-content">
-                                        <p>Olá Sr. Usuário, tudo bem? Gostaria de entender melhor</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="navbar-item">
-                                <a class="li-content">
-                                    <div class="title">
-                                        <span class="material-icons icon">
-                                             person
-                                        </span>
-                                        <span class="username">Usuário 2</span>
-                                    </div>
-                                    <div class="text-content">
-                                        <p>Sr. Usuário, gostaria de solicitar um documento importante</p>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="notifications-footer">
-                            <a href="#">Ver mais</a>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+        <style>
+            @import "css/style.css";
+        </style>
+    </head>
+    <body class="login-container">
+        <section class="fluid-container login-container">
+            <div class="container login-container">
+                <div class="text-center mb-5">
+                    <img src="imgs/imo-control-br.png" />
+                </div>
+                <div class="row">
+                    <form>
+                        <div class="row mb-3">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputEmail3" required>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li class="nav-item notificate-grid">
-                    <span class="material-icons icon">
-                        circle_notifications
-                    </span>
-                    <span class="notifications">
-                        2
-                    </span>
-                    <div class="notifications-container">
-                        <ul class="navbar-nav nav-notifications">
-                            <li class="navbar-item">
-                                <a class="li-content">
-                                    <div class="title">
-                                        <span class="material-icons icon">
-                                             circle_notifications
-                                        </span>
-                                        <span class="username">Usuário 1</span>
-                                    </div>
-                                    <div class="text-content">
-                                        <p>Olá Sr. Usuário, tudo bem? Gostaria de entender melhor</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="navbar-item">
-                                <a class="li-content">
-                                    <div class="title">
-                                        <span class="material-icons icon">
-                                             circle_notifications
-                                        </span>
-                                        <span class="username">Usuário 2</span>
-                                    </div>
-                                    <div class="text-content">
-                                        <p>Sr. Usuário, gostaria de solicitar um documento importante</p>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="notifications-footer">
-                            <a href="#">Ver mais</a>
+                        <div class="row mb-3">
+                            <label for="inputPassword3" class="col-sm-2 col-form-label">Senha</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="inputPassword3" required>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li class="nav-item notificate-grid">
-                    <span class="material-icons icon">
-                        person
-                    </span>
-                    <span class="notifications">
-                        9
-                    </span>
-                    <div class="notifications-container">
-                        <ul class="navbar-nav nav-notifications">
-                            <li class="navbar-item">
-                                <a class="li-content">
-                                    <div class="title">
-                                        <span class="material-icons icon">
-                                             person
-                                        </span>
-                                        <span class="username">Usuário 1</span>
-                                    </div>
-                                    <div class="text-content">
-                                        <p>Olá Sr. Usuário, tudo bem? Gostaria de entender melhor</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="navbar-item">
-                                <a class="li-content">
-                                    <div class="title">
-                                        <span class="material-icons icon">
-                                             person
-                                        </span>
-                                        <span class="username">Usuário 2</span>
-                                    </div>
-                                    <div class="text-content">
-                                        <p>Sr. Usuário, gostaria de solicitar um documento importante</p>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="notifications-footer">
-                            <a href="#">Ver mais</a>
+                        <div class="row mb-3">
+                            <div class="col-sm-10 offset-sm-2">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        lembrar senha
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<header id="navbar_top" class="navbar navbar-expand-lg navbar-light bg-light-imo flex-column flex-md-row">
-    <div class="container justify-content-md-center col-9">
-        <a class="navbar-brand" href="#">
-            <img src="imgs/imo-control-br.png" alt="" width="30" height="24">
-        </a>
-        <div class="collapse navbar-collapse" id="header-main-top">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/homeicon.svg" />
-                        </div>
-                        <span>Início</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/calendarioicon.svg" />
-                        </div>
-                        <span>Calendário</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/certificadosicon.svg" />
-                        </div>
-                        <span>Certificados</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/financeiroicon.svg" />
-                        </div>
-                        <span>Financeiro</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/documentosicon.svg" />
-                        </div>
-                        <span>Documentos</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/statusicon.svg" />
-                        </div>
-                        <span>Status</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link scrollto " aria-current="page" href="index.php#home">
-                        <div class="image icons">
-                            <img src="imgs/contatoicon.svg" />
-                        </div>
-                        <span>Contato</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</header>
+                        <button type="submit" class="btn">Acessar</button>
+                    </form>
 
+                </div>
+            </div>
+        </section>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="js/main.js"></script>
-
-
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="js/main.js"></script>
+    </body>
 </html>
---
+++ SISTEMA/termos.php
@@ -0,0 +1,126 @@
+<?php require_once("header.php"); ?>
+
+    <section class="fluid-container p-2">
+        <div class="container justify-content-md-center col-9">
+            <div class="row">
+                <p class="text-center"> Faça sua certificação em <span class="strong-title">6 etapas</span> </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="fluid-container pt-2">
+        <div class="container justify-content-md-center col-9">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">Cadastra</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="big-number">1</span>
+                            </div>
+                            <div class="col-md-9">
+                                <ul class="main-blocks">
+                                    <li><a href="#">Organização</a></li>
+                                    <li><a href="#">Representantes</a></li>
+                                    <li><a href="#">Unidades</a></li>
+                                    <li><a href="#">Regulamentos</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">ORÇA</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="big-number">2</span>
+                            </div>
+                            <div class="col-md-9">
+                                <ul class="main-blocks">
+                                    <li><a href="#">Solicitação</a></li>
+                                    <li><a href="#">Oferta</a></li>
+                                    <li><a href="#">Contrato</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">PLANEJA</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="big-number">3</span>
+                            </div>
+                            <div class="col-md-9">
+                                <ul class="main-blocks">
+                                    <li><a href="#">Plano de Manej</a></li>
+                                    <li><a href="#">Anexos</a></li>
+                                    <li><a href="#">Dados Básicos</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">AVALIA</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="big-number">4</span>
+                            </div>
+                            <div class="col-md-9">
+                                <ul class="main-blocks">
+                                    <li><a href="#">Carta Comentário </a></li>
+                                    <li><a href="#">Plano Avaliação</a></li>
+                                    <li><a href="#">Inspeção</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">REVISA</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="big-number">5</span>
+                            </div>
+                            <div class="col-md-9">
+                                <ul class="main-blocks">
+                                    <li><a href="#">Não Conformidades</a></li>
+                                    <li><a href="#">Revisão</a></li>
+                                    <li><a href="#">Apelação</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-item-block p-3">
+                        <h3 class="mb-2">CERTIFICA</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <span class="big-number">6</span>
+                            </div>
+                            <div class="col-md-9">
+                                <ul class="main-blocks">
+                                    <li><a href="#">Sumário da Revisão</a></li>
+                                    <li><a href="#">Correções</a></li>
+                                    <li><a href="#">Certificação</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php require_once("footer.php"); ?>
